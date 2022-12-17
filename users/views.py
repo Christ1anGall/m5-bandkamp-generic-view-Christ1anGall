993057293831b90438bb3d1762c94169d8d1b469
Christ1anGall <christiangall92@icloud.com>
2022-12-17 19:44:21 -0300

    feat user

--- users/views.py
+++ users/views.py
@@ -3,40 +3,18 @@ from .models import User
 from .serializers import UserSerializer
 from django.shortcuts import get_object_or_404
 from utils.common_views import GetPostCommonsView
-from utils.detail_common_views import OnlyGetDetailView
+from utils.detail_common_views import (
+    OnlyGetDetailView,
+    OnlyPatchDetailView,
+    OnlyDeleteDetailView,
+)
 
 
 class UserView(GetPostCommonsView):
     view_serializer = UserSerializer
 
 
-class UserDetailView(OnlyGetDetailView):
+class UserDetailView(OnlyGetDetailView, OnlyPatchDetailView, OnlyDeleteDetailView):
 
     view_serializer = UserSerializer
     view_queryset = User.objects.all()
-
-    def patch(self, request: Request, pk: int) -> Response:
-        """
-        Atualização de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        serializer = UserSerializer(user, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data)
-
-    def delete(self, request: Request, pk: int) -> Response:
-        """
-        Deleçao de usuário
-        """
-        user = get_object_or_404(User, pk=pk)
-
-        self.check_object_permissions(request, user)
-
-        user.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
--- utils/detail_common_views.py
+++ utils/detail_common_views.py
@@ -17,6 +17,43 @@ class GetDetailView(APIView):
         return Response(serializer.data, status.HTTP_200_OK)
 
 
+class PatchDetailView:
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    def update(self, request: Request, pk: int) -> Response:
+        model_obj = get_object_or_404(self.view_queryset, pk=pk)
+
+        self.check_object_permissions(request, model_obj)
+
+        serializer = self.view_serializer(model_obj, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+
 class OnlyGetDetailView(GetDetailView, APIView):
     def get(self, request: Request, pk: int) -> Response:
         return super().retrieve(request, pk)
+
+
+class OnlyPatchDetailView(PatchDetailView, APIView):
+    def patch(self, request: Request, pk: int) -> Response:
+        return super().update(request, pk)
+
+
+class DeleteDetailView:
+    def remove(self, request: Request, pk: int) -> Response:
+        model_obj = get_object_or_404(self.view_queryset, pk=pk)
+
+        self.check_object_permissions(request, model_obj)
+
+        model_obj.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class OnlyDeleteDetailView(DeleteDetailView, APIView):
+    def delete(self, request: Request, pk: int) -> Response:
+        return super().remove(request, pk)

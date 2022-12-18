0804c4e0ff3cdf56e6ec81844a816b5303a25f17
Christ1anGall <christiangall92@icloud.com>
2022-12-17 22:39:57 -0300

    feat all

--- albums/serializers.py
+++ albums/serializers.py
@@ -7,12 +7,3 @@ class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
         fields = ["id", "name", "year", "user_id"]
-
-    """     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    year = serializers.IntegerField()
-    user_id = serializers.IntegerField(read_only=True) 
-
-    def create(self, validated_data):
-        return Album.objects.create(**validated_data)
-    """
